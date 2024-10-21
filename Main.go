c73d095db67160c91ef1630c29921cd0a642baca
Luhur65 <dharmabakti1202@gmail.com>
2024-10-21 21:27:23 +0700

    #2 Refactor code Loops to Recursive func

--- Main.go
+++ Main.go
@@ -5,10 +5,56 @@ import (
 	"math/rand"
 )
 
+func checkAnswer(answer, result int) string {
+	if result == answer {
+		return "Benar!"
+	} else {
+		return "Salah!"
+	}
+}
+
+func MathQuiz(soal, answer int) {
+
+	var is_again string
+
+	// cek jika sudah 10 soal
+	if soal > 10 {
+		fmt.Print("Mau main lagi?[Y/N] : ")
+		fmt.Scan(&is_again)
+	}
+
+	if is_again == "n" || is_again == "N" {
+		fmt.Println("Terima kasih sudah bermain ya!")
+	}
+
+	if is_again == "y" || is_again == "Y" {
+		soal = 1 // reset lagi soal
+		MathQuiz(soal, answer)
+	}
+
+	num1 := rand.Intn(10)
+	num2 := rand.Intn(10)
+	result := num1 + num2
+
+	fmt.Println(soal,". ",num1," + ",num2," = ")
+	fmt.Print("Jawaban : ")
+	fmt.Scan(&answer)
+
+	fmt.Println(checkAnswer(answer, result))
+
+	fmt.Println("Jawaban benar = ", result)
+	fmt.Println()
+	soal += 1
+
+	MathQuiz(soal, answer)
+}
+
 func main() {
 
 	var name string
-	var num1, num2, answer int
+	// var num1, num2, answer int
+	var answer int
+	var soal int = 1
 
 	fmt.Print("Enter your name: ")
 	fmt.Scan(&name)
@@ -16,27 +62,8 @@ func main() {
 	fmt.Println("Math Quiz By Golang")
 	fmt.Println("Welcome, ", name)
 	fmt.Println()
-	
-	for i := 1; i <= 10; i++ {
 
-		num1 = rand.Intn(10)
-		num2 = rand.Intn(10)
-
-		result := num1 + num2
-		fmt.Println(i,". ",num1," + ",num2," = ")
-		fmt.Print("Jawaban : ")
-		fmt.Scan(&answer)
-
-		if result == answer {
-			fmt.Println("Benar!")
-		} else {
-			fmt.Println("Salah!")
-			fmt.Println("Jawaban benar = ", result)
-		}
-
-		fmt.Println()
-
-	}
+	MathQuiz(soal, answer)
 
 	fmt.Println("Hebat!, kamu berhasil")
 	fmt.Scan()
